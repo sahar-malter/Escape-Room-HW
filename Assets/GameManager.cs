0da522ee08108e30bc6527db33dfd9e642560878
sahar <sahar.malter1@gmail.com>
2020-12-21 11:57:12 +0200

    good things

---
+++ Assets/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> inventory;
+    public static GameObject Current_Item;
+    public List<Button> button_inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+    public void select_current_item(int index)
+    {
+        if (inventory[index] != null)
+        {
+            Current_Item = inventory[index];
+            Debug.Log(inventory[index].name + " is now the current item");
+        }
+        else 
+        { 
+            Current_Item = null;
+        }
+    }
+
+    public void AddToInventory(GameObject picked_item)
+    {
+        inventory.Add(picked_item);
+        Debug.Log(picked_item + " was added to the inventory");
+        button_inventory[inventory.Count-1].GetComponent<Image>().color = picked_item.GetComponent<MeshRenderer>().material.color;
+    }
+    public void RemoveFromInventory(GameObject used_item)
+    {
+        inventory.Remove(used_item);
+        Debug.Log(used_item + " was removed from the inventory");
+    }
+}
---
+++ Assets/Scripts/Object.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public string name;
+    public bool IsPickable;
+    public bool IsComplexed;
+    public bool IsInteractable;
+
+    private bool IsSolved;
+
+    public GameObject required_item;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsSolved = false;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void picked()
+    {
+        if (IsPickable)
+        {
+        Debug.Log(name + " was picked");
+            gameManager.AddToInventory(gameObject);
+            gameObject.SetActive(false);
+        //do func to add to inventory and remove from game view
+        }
+    }
+    public void interact()
+    {
+        if(IsInteractable && GameManager.Current_Item == required_item || IsSolved)
+        {
+            Debug.Log(required_item.name + " was used" + name + " preformed an interaction");
+            IsSolved = true;
+            //do somthing
+        }
+        else
+        {
+            Debug.Log("cant touch this");
+        }
+        // use animator or leantween to perform some action in the game
+    }
+
+    public void complex_interact()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if(IsPickable && !IsInteractable && !IsComplexed)
+        {
+            picked();
+        }
+        if(!IsPickable && IsInteractable && !IsComplexed)
+        {
+            interact();
+        }
+        if (!IsPickable && !IsInteractable && IsComplexed)
+        {
+            complex_interact();
+        }
+    }
+}
+
+
+
